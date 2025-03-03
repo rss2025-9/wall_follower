858d4adf59082ae4a9886a7c35ad9020c8ae0604
selinnal <selinnalin0705@gmail.com>
2025-03-03 22:50:39 +0000

    changed drive topic x2

--- wall_follower_sim/wall_follower/wall_follower.py
+++ wall_follower_sim/wall_follower/wall_follower.py
@@ -26,7 +26,7 @@ class WallFollower(Node):
         # Declare parameters to make them available for use
         # DO NOT MODIFY THIS! 
         self.declare_parameter("scan_topic", "/scan")
-        self.declare_parameter("drive_topic", "/vesc/high_level/ackermann_cmd")
+        self.declare_parameter("drive_topic", "/vesc/high_level/input/nav_0")
         self.declare_parameter("side", -1)
         self.declare_parameter("velocity", 1.0)
         self.declare_parameter("desired_distance", 1.0)
