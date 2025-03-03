a32793730aaea57748153ae8a05d681b42f9cd00
selinnal <selinnalin0705@gmail.com>
2025-03-03 22:05:50 +0000

    modified wall follower

--- wall_follower_sim/wall_follower/wall_follower.py
+++ wall_follower_sim/wall_follower/wall_follower.py
@@ -25,11 +25,11 @@ class WallFollower(Node):
 
         # Declare parameters to make them available for use
         # DO NOT MODIFY THIS! 
-        self.declare_parameter("scan_topic", "default")
-        self.declare_parameter("drive_topic", "default")
-        self.declare_parameter("side", "default")
-        self.declare_parameter("velocity", "default")
-        self.declare_parameter("desired_distance", "default")
+        self.declare_parameter("scan_topic", "/scan")
+        self.declare_parameter("drive_topic", "/vesc/low_level/ackermann_cmd")
+        self.declare_parameter("side", -1)
+        self.declare_parameter("velocity", 1.0)
+        self.declare_parameter("desired_distance", 1.0)
 
 
         # Fetch constants from the ROS parameter server
