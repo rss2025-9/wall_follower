fd593a5030c2df1ce6e9ee0f801b36ae1068335d
Selinna <selinnalin0705@gmail.com>
2025-03-10 18:49:37 -0400

    artemis wall follower + trying to figure out why it runs on its own -s

--- wall_follower/wall_follower.py
+++ wall_follower/wall_follower.py
@@ -6,40 +6,32 @@ from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 from visualization_msgs.msg import Marker
 from rcl_interfaces.msg import SetParametersResult
-import csv 
-import os
-import pandas as pd 
+import os 
+import csv
 
 from wall_follower.visualization_tools import VisualizationTools
 
-import math 
-
-# Helper functions 
-def angle_to_index(angle, scan):
-    # get an index of scan from a desired angle 
-    clamped_angle = max(scan.angle_min, min(angle, scan.angle_max))
-    idx = int(round((clamped_angle - scan.angle_min) / scan.angle_increment))
-    # idx = max(0, min(idx, len(scan.ranges) - 1))
-    return idx
-
-def polar_to_cartesian(r, theta):
-    x = r * np.cos(theta)
-    y = r * np.sin(theta)
-    return x, y
 
 class WallFollower(Node):
 
     def __init__(self):
         super().__init__("wall_follower")
 
-        # ROS parameters 
+        #ADDED gains
+        self.kp=1.0 #3 (12)
+        self.kd=0.5 #1.5 (4)
+        #ki (10)
+
+        self.prev_error = 0.0
+
+        # Declare parameters to make them available for use
+        # DO NOT MODIFY THIS! 
         self.declare_parameter("scan_topic", "/scan")
-        self.declare_parameter("drive_topic", "/vesc/high_level/input/nav_0")
+        self.declare_parameter("drive_topic", "/vesc/high_level/input/nav_1")
         self.declare_parameter("side", -1)
-        self.declare_parameter("velocity", 2.0)
+        self.declare_parameter("velocity", 1)
         self.declare_parameter("desired_distance", 0.5)
         self.declare_parameter('using_real_car', True)
-        self.declare_parameter('min_turn_radius', 0.5) # min turn radius of car in meters
 
         # Fetch constants from the ROS parameter server
         # DO NOT MODIFY THIS! This is necessary for the tests to be able to test varying parameters!
@@ -48,34 +40,20 @@ class WallFollower(Node):
         self.SIDE = self.get_parameter('side').get_parameter_value().integer_value
         #self.SIDE = 1 #-1 right
         self.VELOCITY = self.get_parameter('velocity').get_parameter_value().double_value
-        self.DESIRED_DISTANCE = self.get_parameter('desired_distance').get_parameter_value().double_value 
-        self.RADIUS = self.get_parameter('min_turn_radius').get_parameter_value().double_value 
-		
+        self.DESIRED_DISTANCE = self.get_parameter('desired_distance').get_parameter_value().double_value      
+
         # This activates the parameters_callback function so that the tests are able
         # to change the parameters during testing.
         # DO NOT MODIFY THIS! 
-        self.add_on_set_parameters_callback(self.parameters_callback)
-  
-        # Subscribers and Publishers
-        self.can_subscriber = self.create_subscription(LaserScan, self.SCAN_TOPIC, self.scan_callback, 1)
-        self.drive_publisher = self.create_publisher(AckermannDriveStamped, self.DRIVE_TOPIC, 1)
+        # self.add_on_set_parameters_callback(self.parameters_callback)
 
-        # publisher for the visualization of the wall follower
-        self.view_publisher = self.create_publisher(LaserScan, "view_topic", 1)
-        self.wall_publisher = self.create_publisher(Marker, "wall_topic", 1)
-        self.steer_publisher = self.create_publisher(Marker, "steer_topic", 1)
-
-
-        # variables to store previous results 
-        self.prev_error = 0
-        self.integral = 0
-        self.prev_time = self.get_clock().now()
-        self.prev_line = None
-
-        # PID control variables
-        self.Kp = 1
-        self.Ki = 0.0
-        self.Kd = 0.5
+        #ADDED subscribe to lidar data (subscibe to SCN_TOPIC), listens for messages of type LaserScan, when msg arrives lidar_callback called
+        self.subscription=self.create_subscription(LaserScan, self.SCAN_TOPIC, self.lidar_callback, 1)
+        self.subscription 
+        #ADDED publish drive,  publishes msgs of type AckermannDriveStamped to topic DRIVE_TOPIC
+        self.drive_pub=self.create_publisher(AckermannDriveStamped, self.DRIVE_TOPIC, 10)
+        # a publisher for our line marker
+        self.line_pub = self.create_publisher(Marker, "wall", 1)
 
         # File for wall follower data collection
         self.wall_follower_data = "wall_follower_data.csv"
@@ -85,111 +63,109 @@ class WallFollower(Node):
                 writer.writerow(["Time (s)", "Desired Distance (m)", "Estimated Distance to Wall (m)", "Error (m)", "Velocity (m/s)"])
 
         self.wall_follower_data_records = []
+        self.current_time = 0
 
-    # Callback function for the scan subscriber
-    def scan_callback(self, scan):  
+    # TODO: Write your callback functions here   
+    #ADDED
+    def lidar_callback(self, msg):
+        if not msg.ranges: #no lidar data received
+            self.get_logger().warn("No LIDAR data")
+            return
 
-        # Print messages =================================================================
-        # self.get_logger().info("scan min angle: " + str(scan.angle_min))
-        # self.get_logger().info("scan max angle: " + str(scan.angle_max))
-        # self.get_logger().info("scan angle increment: " + str(scan.angle_increment))
+        #to np array
+        msg_arr=np.array(msg.ranges)
+        angles=np.linspace(msg.angle_min, msg.angle_max, len(msg_arr))
 
-        # Publish what the robot is seeing ============================================================
-        view_msg = LaserScan()
-        view_msg.header = scan.header
-        # view_msg.angle_min = scan.angle_min
-        # view_msg.angle_max = scan.angle_max
-        view_msg.angle_increment = scan.angle_increment
-        view_msg.time_increment = scan.time_increment
-        view_msg.scan_time = scan.scan_time
-        view_msg.range_min = scan.range_min
-        view_msg.range_max = scan.range_max - 1.0
-        # view_msg.ranges = scan.ranges
-        view_msg.intensities = scan.intensities
-
-        # Slice up the scan data in half depending on side of wall we are following ==========================
-        # side = 1 means we are following the left wall, -1 means we are following the right wall
-
-        if self.SIDE == -1:
-            start_angle = -np.pi*5/8
-            end_angle = np.pi/8
-        else:
-            start_angle = -np.pi/8
-            end_angle = np.pi*5/8
-
-        start_idx = angle_to_index(start_angle, scan)
-        end_idx = angle_to_index(end_angle, scan)
-
-        if start_idx > end_idx:
-            start_idx, end_idx = end_idx, start_idx
+        # angle range
+        ang_min=-np.pi/8 if self.SIDE==1 else -np.pi*3/8
+        ang_max=np.pi*3/8 if self.SIDE==1 else np.pi/8
         
-        view_msg.angle_min = start_angle 
-        view_msg.angle_max = end_angle
-        view_msg.ranges = scan.ranges[start_idx:end_idx]
-        self.view_publisher.publish(view_msg)
 
-        ranges = np.array(scan.ranges[start_idx:end_idx])
-        indices = np.arange(start_idx, end_idx)
-        angles = scan.angle_min + indices * scan.angle_increment
-        mask = np.isfinite(ranges) & (ranges >= scan.range_min) & (ranges <= scan.range_max) & (angles >= scan.angle_min) & (angles <= scan.angle_max)
-        valid_ranges = ranges[mask]
-        valid_angles = angles[mask]
+        #indices of this range
+        mask=(angles>=ang_min) & (angles<=ang_max)
+        selected_angles=angles[mask]
+        selected_ranges=msg_arr[mask]
 
-        if len(valid_ranges) == 0:
-            distance = self.DESIRED_DISTANCE
-            angle = 0
-        else: 
-            x, y = polar_to_cartesian(valid_ranges, valid_angles)
-            slope, intercept = np.polyfit(x, y, 1, w = 1 + (1/valid_ranges))
-            VisualizationTools.plot_line(x, slope * x + intercept, self.wall_publisher, frame="/laser")
-            distance = abs(intercept)/math.sqrt(1 + slope**2)
-            angle = np.arctan(slope)
+        #get rid of bad readings
+        #valid_mask=(selected_ranges>msg.range_min) & (selected_ranges<msg.range_max) 
+        valid_mask=(selected_ranges>msg.range_min) & (selected_ranges<4.0) 
+        selected_angles=selected_angles[valid_mask]
+        selected_ranges=selected_ranges[valid_mask]
 
-        error = (self.DESIRED_DISTANCE) - distance
+        if len(selected_ranges) < 2:
+            self.get_logger().warn("not enough data")
+            return
 
-        # Compute time difference ======================================================
-        current_time = self.get_clock().now()
-        dt = (current_time - self.prev_time).nanoseconds / 1e9
-        if dt == 0:
-            dt = 1e-6  # avoid division by zero
+        #to cartesian
+        x_vals=selected_ranges*np.cos(selected_angles)
+        y_vals=selected_ranges*np.sin(selected_angles)
 
-        # PID ================================================================================
-        self.integral += error * dt
-        derivative = (error - self.prev_error) / dt
-        control = self.Kp * error + self.Ki * self.integral + self.Kd * derivative * (0.75 + 0.25*error/(error + self.RADIUS))
-        steering_angle = -self.SIDE * control + angle
+        #fit line
+        slope, y_int=np.polyfit(x_vals, y_vals, 1)
 
-        VisualizationTools.plot_line([0.0, np.cos(steering_angle)], [0.0, np.sin(steering_angle)], self.steer_publisher, frame="/laser", color = (1.0, 1.0, 0.0))
-        # self.get_logger().info(f"Steering={steering_angle*180/np.pi}, Error = {error} m")
+        #show hypothesized wall
+        x = np.linspace(-2., 2., num=20)
+        y = slope*x +y_int
+        VisualizationTools.plot_line(x, y, self.line_pub, frame="/laser")
 
-        # update for next iteration 
-        self.prev_error = error 
-        self.prev_time = current_time
+        #perp dist from car to line
+        dist_to_wall=abs(y_int)/np.sqrt(slope**2+1)
 
-        # Publish drive message ==================================================
-        drive_msg = AckermannDriveStamped()
-        drive_msg.header.stamp = self.get_clock().now().to_msg()
-        drive_msg.header.frame_id = "laser"
-        drive_msg.drive.steering_angle = steering_angle
-        drive_msg.drive.speed = self.VELOCITY
-        self.drive_publisher.publish(drive_msg) 
+        #find error
+        error=self.DESIRED_DISTANCE-dist_to_wall #FLIP?
+
+        #compute derivative
+        dt=max(msg.scan_time, 0.05)  #so no /0
+        der=(error-self.prev_error)/dt
+
+        #regular PD control
+        if self.SIDE==-1: #if right side
+            steer_ang=self.kp*error+self.kd*slope 
+        else:
+            steer_ang=-(self.kp*error+self.kd*-slope)
+
+        self.publish_drive_command(steer_ang)
+        self.prev_error=error
 
         # Writing to wall follower data spreadsheet
-        self.wall_follower_data_records.append([current_time, self.DESIRED_DISTANCE, distance, error, self.VELOCITY])
+        self.wall_follower_data_records.append([self.current_time, self.DESIRED_DISTANCE, dist_to_wall, error, self.VELOCITY])
+        self.current_time += 1 
 
         # Write to file every 10 records
         if len(self.wall_follower_data_records) >= 10:
             self.write_to_csv()
 
-        # print desired vs results 
-        # self.get_logger().info(f"Side: {self.SIDE}")
-        # self.get_logger().info(f"Desired distance: {self.DESIRED_DISTANCE}, Actual distance: {distance}, Error: {error}, Steering angle: {steering_angle}")
-    
     def write_to_csv(self): 
         with open(self.wall_follower_data, mode = 'a', newline = '') as file: 
             writer = csv.writer(file)
             writer.writerows(self.wall_follower_data_records)
         self.wall_follower_data_records = []
+
+    def publish_drive_command(self, steer_ang):
+        #max_steer_ang=0.5  #something reasonable
+        #steer_ang=np.clip(steer_ang, -max_steer_ang, max_steer_ang) #limits range of steering ang to [-max,max]
+        # msg=AckermannDriveStamped() #message of type AckermannDriveStamped()
+        # #set
+        # base_speed = self.VELOCITY 
+        # min_speed = 1.0 
+        # max_speed = 3.0
+        # steer_thresh = 0.3
+
+        # if abs(steer_ang) > steer_thresh:
+        #     drive_speed = max(min_speed, base_speed * (1 - abs(steer_ang)))
+        # else:
+        #     drive_speed = min(max_speed, base_speed * 1.2)
+        # msg.drive.speed = drive_speed 
+        # msg.drive.steering_angle=steer_ang
+        # #publish msg (steering ang an speed) to /drive topic
+        # self.drive_pub.publish(msg)
+
+        msg=AckermannDriveStamped() #message of type AckermannDriveStamped()
+        #set
+        msg.drive.speed=self.VELOCITY
+        msg.drive.steering_angle=steer_ang
+        #publish msg (steering ang an speed) to /drive topic
+        self.drive_pub.publish(msg)
     
     def parameters_callback(self, params):
         """
@@ -198,8 +174,6 @@ class WallFollower(Node):
         This is used by the test cases to modify the parameters during testing. 
         It's called whenever a parameter is set via 'ros2 param set'.
         """
-        # while(True):
-        #     self.get_logger().info(f"blocked")
         for param in params:
             if param.name == 'side':
                 self.SIDE = param.value
