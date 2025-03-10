410dbdbdb1d2debfd313669f42d4d1f5f059c16d
Selinna <selinnalin0705@gmail.com>
2025-03-10 18:03:39 -0400

    Added data collection code -s

--- wall_follower/wall_follower.py
+++ wall_follower/wall_follower.py
@@ -6,6 +6,9 @@ from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 from visualization_msgs.msg import Marker
 from rcl_interfaces.msg import SetParametersResult
+import csv 
+import os
+import pandas as pd 
 
 from wall_follower.visualization_tools import VisualizationTools
 
@@ -24,32 +27,6 @@ def polar_to_cartesian(r, theta):
     y = r * np.sin(theta)
     return x, y
 
-# Is this ever used???
-def ransac(points, num_iterations = 4, threshold = 0.1, min_inlier_ratio = 0.5): 
-    iterations = 0 
-    best_inlier_count = 0
-    best_fit = None
-    num_points = len(points)
-    while iterations < num_iterations: 
-        if num_points < 2:
-            return None, 0
-        indices = np.random.choice(num_points, 2, replace=False)
-        p1, p2 = points[indices[0]], points[indices[1]]
-        # Find line (x1, y1) and (x2, y2) that fits the data
-        A = p2[1] - p1[1]
-        B = p1[0] - p2[0]
-        C = p2[0]*p1[1] - p1[0]*p2[1]
-        # Compute distance of each point to the line
-        distances = np.abs(A * points[:, 0] + B * points[:, 1] + C) / np.sqrt(A**2 + B**2)
-        inliers = distances < threshold
-        inlier_count = np.sum(inliers)
-
-        if inlier_count > best_inlier_count and inlier_count > min_inlier_ratio * num_points:
-            best_inlier_count = inlier_count
-            best_fit = (A, B, C)
-        iterations += 1
-    return best_fit, best_inlier_count
-
 class WallFollower(Node):
 
     def __init__(self):
@@ -58,8 +35,8 @@ class WallFollower(Node):
         # ROS parameters 
         self.declare_parameter("scan_topic", "/scan")
         self.declare_parameter("drive_topic", "/vesc/high_level/input/nav_0")
-        self.declare_parameter("side", 1)
-        self.declare_parameter("velocity", 1.0)
+        self.declare_parameter("side", -1)
+        self.declare_parameter("velocity", 2.0)
         self.declare_parameter("desired_distance", 0.5)
         self.declare_parameter('using_real_car', True)
         self.declare_parameter('min_turn_radius', 0.5) # min turn radius of car in meters
@@ -77,7 +54,7 @@ class WallFollower(Node):
         # This activates the parameters_callback function so that the tests are able
         # to change the parameters during testing.
         # DO NOT MODIFY THIS! 
-        # self.add_on_set_parameters_callback(self.parameters_callback)
+        self.add_on_set_parameters_callback(self.parameters_callback)
   
         # Subscribers and Publishers
         self.can_subscriber = self.create_subscription(LaserScan, self.SCAN_TOPIC, self.scan_callback, 1)
@@ -99,6 +76,15 @@ class WallFollower(Node):
         self.Kp = 1
         self.Ki = 0.0
         self.Kd = 0.5
+
+        # File for wall follower data collection
+        self.wall_follower_data = "wall_follower_data.csv"
+        if not os.path.exists(self.wall_follower_data):
+            with open(self.wall_follower_data, mode="w", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow(["Time (s)", "Desired Distance (m)", "Estimated Distance to Wall (m)", "Error (m)", "Velocity (m/s)"])
+
+        self.wall_follower_data_records = []
 
     # Callback function for the scan subscriber
     def scan_callback(self, scan):  
@@ -130,7 +116,6 @@ class WallFollower(Node):
         else:
             start_angle = -np.pi/8
             end_angle = np.pi*5/8
-            
 
         start_idx = angle_to_index(start_angle, scan)
         end_idx = angle_to_index(end_angle, scan)
@@ -154,55 +139,11 @@ class WallFollower(Node):
             distance = self.DESIRED_DISTANCE
             angle = 0
         else: 
-            # distance = np.mean(valid_ranges)
             x, y = polar_to_cartesian(valid_ranges, valid_angles)
             slope, intercept = np.polyfit(x, y, 1, w = 1 + (1/valid_ranges))
             VisualizationTools.plot_line(x, slope * x + intercept, self.wall_publisher, frame="/laser")
             distance = abs(intercept)/math.sqrt(1 + slope**2)
             angle = np.arctan(slope)
-            # angle = 0
-
-        # checking if there's a wall upcoming at front 
-        # mid_start = angle_to_index(-math.pi/15, scan)
-        # mid_end = angle_to_index(math.pi/15, scan)
-        # mid_ranges = np.array(scan.ranges[mid_start: mid_end])
-        # valid_mid_ranges = np.isfinite(mid_ranges) 
-
-        # turn_offset = 0
-        # if len(valid_mid_ranges) != 0: 
-        #     if self.VELOCITY <= 1 and np.mean(valid_mid_ranges) < 0.75: 
-        #         turn_offset = 0.25
-        #     elif self.VELOCITY <= 2 and np.mean(valid_mid_ranges) < 1.25: 
-        #         turn_offset = 0.3
-        #     elif self.VELOCITY <= 3 and np.mean(valid_mid_ranges) < 2: 
-        #         turn_offset = 0.4 
-        
-        # Attempt to implementing RANSAC :( ==================================================
-        # points = []
-        # for r, theta in zip(valid_ranges, valid_angles):
-        #     x, y = polar_to_cartesian(r, theta)
-        #     points.append([x, y])
-        # points = np.array(points)
-
-        # line, num_inliers = ransac(points, num_iterations = 4, threshold = 0.1, min_inlier_ratio = 0.5)
-        # if num_inliers == 0: 
-        #     self.get_logger().info("No inliers found")
-        #     if self.prev_line is not None: 
-        #         A, B, C = self.prev_line
-        #         distance = abs(C) / np.sqrt(A**2 + B**2)
-        #     else: 
-        #         distance = np.mean(valid_ranges) if len(valid_ranges) > 0 else self.DESIRED_DISTANCE
-        # else: 
-        #     A, B, C = line 
-        #     distance = abs(C) / np.sqrt(A**2 + B**2)
-        #     self.prev_line = line
-        
-        # if line is not None: 
-        #     x_min = np.min(points[:, 0])
-        #     x_max = np.max(points[:, 0])
-        #     x = np.linspace(x_min, x_max, num=20) 
-        #     y = (-A * x - C) / B 
-        #     VisualizationTools.plot_line(x, y, self.wall_publisher, frame="base_link")
 
         error = (self.DESIRED_DISTANCE) - distance
 
@@ -218,11 +159,8 @@ class WallFollower(Node):
         control = self.Kp * error + self.Ki * self.integral + self.Kd * derivative * (0.75 + 0.25*error/(error + self.RADIUS))
         steering_angle = -self.SIDE * control + angle
 
-
         VisualizationTools.plot_line([0.0, np.cos(steering_angle)], [0.0, np.sin(steering_angle)], self.steer_publisher, frame="/laser", color = (1.0, 1.0, 0.0))
-        self.get_logger().info(f"Steering={steering_angle*180/np.pi}, Error = {error} m")
-
-
+        # self.get_logger().info(f"Steering={steering_angle*180/np.pi}, Error = {error} m")
 
         # update for next iteration 
         self.prev_error = error 
@@ -236,9 +174,22 @@ class WallFollower(Node):
         drive_msg.drive.speed = self.VELOCITY
         self.drive_publisher.publish(drive_msg) 
 
+        # Writing to wall follower data spreadsheet
+        self.wall_follower_data_records.append([current_time, self.DESIRED_DISTANCE, distance, error, self.VELOCITY])
+
+        # Write to file every 10 records
+        if len(self.wall_follower_data_records) >= 10:
+            self.write_to_csv()
+
         # print desired vs results 
         # self.get_logger().info(f"Side: {self.SIDE}")
         # self.get_logger().info(f"Desired distance: {self.DESIRED_DISTANCE}, Actual distance: {distance}, Error: {error}, Steering angle: {steering_angle}")
+    
+    def write_to_csv(self): 
+        with open(self.wall_follower_data, mode = 'a', newline = '') as file: 
+            writer = csv.writer(file)
+            writer.writerows(self.wall_follower_data_records)
+        self.wall_follower_data_records = []
     
     def parameters_callback(self, params):
         """
@@ -247,6 +198,8 @@ class WallFollower(Node):
         This is used by the test cases to modify the parameters during testing. 
         It's called whenever a parameter is set via 'ros2 param set'.
         """
+        # while(True):
+        #     self.get_logger().info(f"blocked")
         for param in params:
             if param.name == 'side':
                 self.SIDE = param.value
